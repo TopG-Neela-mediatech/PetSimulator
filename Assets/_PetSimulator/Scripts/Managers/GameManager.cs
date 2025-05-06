4b1fa930cfeb21bf04d5c2c8baab89e3282ce9b3
bhaskarpal2611 <bhaskarp@neelafilm.com>
2025-05-06 14:21:33 +0530

    Update GameManager.cs

--- Assets/_PetSimulator/Scripts/Managers/GameManager.cs
+++ Assets/_PetSimulator/Scripts/Managers/GameManager.cs
@@ -5,6 +5,19 @@ namespace TMKOC.PetSimulator
     public class GameManager : GenericSingleton<GameManager>
     {
 
-    }
+        // Game Loop
 
+        // On Game Start - trigger point invoked at start of the game 
+           
+            //  Get Time - Realtime? for day or night lighting Cycle
+            // Spawn and initialize player
+            // Get Status values from player model
+        
+
+
+
+        // Player Spawner Service -> MVC 
+
+        // Pet Status Manager - decrement values of player model and get the values for displaying using UI_Meter 
+    }
 }
